a10b020991a65d8fb1968cd5ec60886508d2c6c3
João Pedro Rosada Volponi <jpvrosada@hotmail.com>
2023-03-30 15:20:39 -0300

    Criação das rotas GET/id e PUT/id

--- src/app.js
+++ src/app.js
@@ -3,18 +3,57 @@ import express from 'express'
 // instancia do express
 const app = express()
 
+// Recurso de interpretação via POST / PUT / armazena e manipula
+app.use(express.json())
+
+
 const livros = [ 
     { id: 1, 'titulo': 'Senhor dos Aneis ' }, 
     { id: 2, 'titulo': ' O Hobiit'},
 ]
 
 // Rotas 200 - Ok
+
+// METODOS
+    // GET
 app.get('/', (req, res) => { 
     res.status(200).send('Curso de Node');
 })
 
+
 app.get('/livros', (req, res) => { 
     res.status(200).json(livros)
 })
+
+    //Buscando livros por ID
+app.get('/livros/:id', (req, res) => { 
+    let index = buscaLivro(req.params.id) // Busca do index
+    res.json(livros[index])  // retorna apenas a posição do index
+})
+
+
+// -----------------------------------------------------
+
+    // POST
+app.post('/livros', (req, res) => { 
+    // BODY corpo da req
+    livros.push(req.body); 
+    res.status(201).send('Livro foi cadastrado com sucesso!') // 201 Criado - OK
+})
+
+// -----------------------------------------------------
+    // PUT
+        // Alterando o nome do livro
+app.put('/livros/:id', (req, res) => { 
+    let index = buscaLivro(req.params.id) 
+        // posicao do livro no array = novo titulo vindo no corpo da requisicao
+    livros[index].titulo = req.body.titulo 
+    res.json(livros)
+})
+
+    function buscaLivro(id) { 
+
+        return livros.findIndex(livro => livro.id == id)
+    }
 
 export default app
