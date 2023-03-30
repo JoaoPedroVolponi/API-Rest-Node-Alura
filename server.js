31cb122dfe21b9566bc50def5901a106c54e66e9
João Pedro Rosada Volponi <jpvrosada@hotmail.com>
2023-03-30 11:27:01 -0300

    Adicionando a biblioteca nodemon

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const http = require("http")  // modulo nativo HTTP
+// nodemon -> Biblioteca -> live reload - a cada alteração e salvar ele identifica e faz o reload
 
+
+const http = require("http")  // modulo nativo HTTP
 const port = 3000; 
+
+const rotas = { 
+    '/':  'Curso de Node',
+    '/livros': 'Entrei na pagina de livros',
+    '/autores': 'Listagem de autores',
+    '/editora': 'Pagina de editora',
+    '/sobre': 'Info sobre o Projeto',
+}
+
 
 const server = http.createServer((req, res) => { 
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Curso de Node');
+    res.end(rotas[req.url]); // resposta /url
 })
 
 server.listen(port, () => { 
