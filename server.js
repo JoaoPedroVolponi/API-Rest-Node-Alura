f860557226e3db827afdc448fab21b71d3339450
João Pedro Rosada Volponi <jpvrosada@hotmail.com>
2023-03-30 14:29:28 -0300

    Criando duas rotas get com express

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
 // nodemon -> Biblioteca -> live reload - a cada alteração e salvar ele identifica e faz o reload
+import app from "./src/app.js";
+
+const port = process.env.PORT || 3000; 
 
 
-const http = require("http")  // modulo nativo HTTP
-const port = 3000; 
 
-const rotas = { 
-    '/':  'Curso de Node',
-    '/livros': 'Entrei na pagina de livros',
-    '/autores': 'Listagem de autores',
-    '/editora': 'Pagina de editora',
-    '/sobre': 'Info sobre o Projeto',
-}
-
-
-const server = http.createServer((req, res) => { 
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end(rotas[req.url]); // resposta /url
-})
-
-server.listen(port, () => { 
+app.listen(port, () => { 
     console.log(`Servidor escutando em http://localhost: ${port}`)
 })
---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+// instancia do express
+const app = express()
+
+const livros = [ 
+    { id: 1, 'titulo': 'Senhor dos Aneis ' }, 
+    { id: 2, 'titulo': ' O Hobiit'},
+]
+
+// Rotas 200 - Ok
+app.get('/', (req, res) => { 
+    res.status(200).send('Curso de Node');
+})
+
+app.get('/livros', (req, res) => { 
+    res.status(200).json(livros)
+})
+
+export default app
